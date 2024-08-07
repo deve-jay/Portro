1be6bef717b5da9ff7e3ea7d6e8d71901cc56eae
Jay <original.jaysharma@gmail.com>
2024-08-07 21:17:56 +0530

    tailwind formater added

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,11 +1,11 @@
 // import React from 'react'
 
-const Background = ({element}:any) => {
+const Background = ({ element }: any) => {
   return (
     <>
       <div className="flex  h-[100vh]">
-        <div className="h-full w-full  bg-black  bg-grid-white/[0.2] absolute flex items-center justify-center"></div>
-        <div className="absolute pointer-events-none inset-0  bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="absolute flex items-center justify-center w-full h-full bg-gray-900 bg-dot-white/15"></div>
+        {/* <div className="absolute pointer-events-none inset-0  bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
         {element}
       </div>
     </>
