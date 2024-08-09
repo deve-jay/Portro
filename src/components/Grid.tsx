c868cb5db22b47bab87e548aed9130b0d12fd540
Jay <original.jaysharma@gmail.com>
2024-08-09 14:14:28 +0530

    roundedness

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,13 +1,13 @@
 const Grid = () => {
   return (
-    <div className="grid grid-cols-12 grid-rows-4 auto-row-[300px] my-40 gap-4 mx-40 relative opacity-10">
-      <div className="h-[200px] col-span-5 bg-blue-500 rounded-md" />
-      <div className="h-[200px] col-span-7 bg-orange-500 rounded-md" />
-      <div className="h-[200px] col-span-2 bg-red-500 rounded-md" />
-      <div className="h-[200px] col-span-7  bg-emerald-500 rounded-md" />
-      <div className="h-[200px] col-span-3 bg-amber-500 rounded-md" />
-      <div className="h-[200px] col-span-9 bg-facebook rounded-md" />
-      <div className="h-[200px] col-span-3 bg-instagram rounded-md" />
+    <div className="grid grid-cols-12 auto-row-[400px] my-20 mx-20 relative opacity-100 rounded-md">
+      <div className="h-[100px] col-span-2 bg-emerald-500/0 border-b-8 border-r-8  rounded-br-3xl border-emerald-500 self-baseline mt-[6.25rem] translate-x-2 translate-y-2" />
+      <div className="h-[200px] col-span-10 bg-emerald-500 rounded-t-lg" />
+      <div className="h-[450px] col-span-4 bg-emerald-500 rounded-tl-lg" />
+      <div className="h-[450px] col-span-5 bg-emerald-500   " />
+      <div className="h-[450px] col-span-3 bg-emerald-500  rounded-br-lg " />
+      <div className="h-[200px] col-span-10 bg-emerald-500  rounded-b-lg " />
+      <div className="h-[200px] col-span-2 bg-emerald-500/0 border-t-8 border-l-8 rounded-tl-3xl mb-[6.25rem] -translate-x-2 -translate-y-2 border-emerald-500" />
     </div>
   );
 };
